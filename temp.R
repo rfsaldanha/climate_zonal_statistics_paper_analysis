38a2558473aca7361acc95f5177d2ed53ca15ccd
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-23 13:01:51 +0200

    mean temp plot

--- temp.R
+++ temp.R
@@ -19,8 +19,8 @@ br_grid <- br_states_grid1 %>%
   left_join(capitals, by = c("code" = "abbrev_state")) %>%
   select(row, col, code, name = name_muni)
 
-temp <- open_dataset(sources = "../brclim2/output_data/parquet/2m_temperature_min.parquet") %>%
-  filter(name == "2m_temperature_min_mean") %>%
+temp <- open_dataset(sources = "../brclim2/output_data/parquet/2m_temperature_mean.parquet") %>%
+  filter(name == "2m_temperature_mean_mean") %>%
   filter(code_muni %in% capitals$code_muni) %>%
   collect() %>%
   right_join(capitals) %>%
@@ -30,12 +30,12 @@ temp <- open_dataset(sources = "../brclim2/output_data/parquet/2m_temperature_mi
   )
 
 ggplot(data = temp, aes(x = date, y = value, group = year, color = year)) +
-  geom_line(lwd = .5, alpha = .3) +
+  geom_line(lwd = .5, alpha = .1) +
   scale_x_date(date_breaks = "1 month", labels = dte_formatter) +
   scale_colour_viridis_c(option = "B") +
   facet_geo(~abbrev_state, grid = br_grid, label = "name") +
   theme_bw() +
   theme(legend.position = "bottom", legend.direction = "horizontal") +
-  labs(x = "Month", y = "Temperature (K)", color = "Year")
+  labs(x = "Month", y = "Mean temperature (K)", color = "Year")
 
 ggsave(filename = "temp_capitals.pdf", width = 210, height = 297, units = "mm")
